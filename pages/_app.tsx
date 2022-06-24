c50250f2e6cc23949a636ed9f6c33bed3e8b6876
kevscript <kevscripted@gmail.com>
2022-06-25 01:36:23 +0200

    added SessionProvider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import Sidebar from "../components/Sidebar";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <div className="w-screen flex flex-row relative">
-      <Sidebar />
-      <div className="flex-1 py-4 px-2">
-        <Component {...pageProps} />
+    <SessionProvider session={session}>
+      <div className="w-screen flex flex-row relative">
+        <Sidebar />
+        <div className="flex-1 py-4 px-2">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </SessionProvider>
   );
 }
 
