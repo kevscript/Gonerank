4126a2dc21e7509dc972f8ecbd776f559d707470
kevscript <kevscripted@gmail.com>
2022-06-25 18:15:41 +0200

    updated Sidebar menu with Icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,28 +3,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRef, useState } from "react";
 import useOutsideClick from "../hooks/useOutsideClick";
+import CalendarIcon from "./Icons/Calendar";
+import ClubIcon from "./Icons/Club";
+import MatchIcon from "./Icons/Match";
 import MoonIcon from "./Icons/Moon";
+import PlayerIcon from "./Icons/Player";
+import RatingIcon from "./Icons/Rating";
+import ShieldIcon from "./Icons/Shield";
 import SignOutIcon from "./Icons/SignOut";
+import TrophyIcon from "./Icons/Trophy";
 import TwitterIcon from "./Icons/Twitter";
 import UserIcon from "./Icons/User";
 
 export type NavRoute = {
   label: string;
   path: string;
+  Icon?: React.ElementType;
 };
 
 const userRoutes: NavRoute[] = [
-  { label: "Home", path: "/" },
-  { label: "Players", path: "/players" },
-  { label: "Matches", path: "/matches" },
+  { label: "Home", path: "/", Icon: RatingIcon },
+  { label: "Players", path: "/players", Icon: PlayerIcon },
+  { label: "Matches", path: "/matches", Icon: MatchIcon },
 ];
 
 const adminRoutes: NavRoute[] = [
-  { label: "Matches", path: "/admin/" },
-  { label: "Players", path: "/admin/players" },
-  { label: "Clubs", path: "/admin/clubs" },
-  { label: "Seasons", path: "/admin/seasons" },
-  { label: "Competitions", path: "/admin/competitions" },
+  { label: "Matches", path: "/admin/", Icon: MatchIcon },
+  { label: "Players", path: "/admin/players", Icon: PlayerIcon },
+  { label: "Clubs", path: "/admin/clubs", Icon: ShieldIcon },
+  { label: "Seasons", path: "/admin/seasons", Icon: CalendarIcon },
+  { label: "Competitions", path: "/admin/competitions", Icon: TrophyIcon },
 ];
 
 type NavigationType = "user" | "admin";
@@ -54,21 +62,29 @@ const Sidebar = () => {
       <MoonIcon className="w-4 h-4 mt-4" />
       <ul className="w-full mt-16 flex-1 flex flex-col items-center">
         {navigation === "user" &&
-          userRoutes.map((r) => (
-            <Link key={r.label} href={r.path} passHref>
+          userRoutes.map(({ label, path, Icon }) => (
+            <Link key={label} href={path} passHref>
               <li className="mt-8 first:mt-0 flex">
-                <div className="w-6 h-6 bg-slate-300"></div>
-                <span className="hidden font-medium ml-4">{r.label}</span>
+                {Icon ? (
+                  <Icon className="w-6 h-6" />
+                ) : (
+                  <div className="w-6 h-6 bg-slate-300"></div>
+                )}
+                <span className="hidden font-medium ml-4">{label}</span>
               </li>
             </Link>
           ))}
 
         {navigation === "admin" &&
-          adminRoutes.map((r) => (
-            <Link key={r.label} href={r.path} passHref>
+          adminRoutes.map(({ label, path, Icon }) => (
+            <Link key={label} href={path} passHref>
               <li className="mt-8 first:mt-0 flex">
-                <div className="w-6 h-6 bg-slate-300"></div>
-                <span className="hidden font-medium ml-4">{r.label}</span>
+                {Icon ? (
+                  <Icon className="w-6 h-6" />
+                ) : (
+                  <div className="w-6 h-6 bg-slate-300"></div>
+                )}
+                <span className="hidden font-medium ml-4">{label}</span>
               </li>
             </Link>
           ))}
