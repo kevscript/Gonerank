9af2f5d497df33042fdc3a117177939cc3f15762
kevscript <kevscripted@gmail.com>
2022-07-03 21:47:24 +0200

    updated Switcher colors

--- components/shared/Switcher.tsx
+++ components/shared/Switcher.tsx
@@ -16,11 +16,11 @@ const Switcher = ({ checked, handleToggle }: SwitcherProps) => {
         id="switcher"
         className={`sr-only peer after:checked:translate-x-full checked:after:border-white checked:after:left-1.5`}
       />
-      <div className="absolute h-4 w-4 rounded-full bg-white border-2 border-gray-200 transition-transform peer-checked:translate-x-full peer-checked:border-blue-200"></div>
+      <div className="absolute h-4 w-4 rounded-full bg-white border-2 border-gray-200 transition-transform peer-checked:translate-x-full peer-checked:border-marine-200"></div>
       <div
         className={`border-2 h-4 w-8 rounded-full ${
           checked
-            ? "bg-blue-600 border-blue-600"
+            ? "bg-marine-600 border-marine-600"
             : "bg-gray-400 border-gray-400  "
         }`}
       ></div>
