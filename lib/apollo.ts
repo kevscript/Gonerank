98a5b31bd547e2012bbb536ef03133bfcbcd80f0
kevscript <kevscripted@gmail.com>
2022-07-09 11:09:06 +0200

    added apollo devtools

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const apolloClient = new ApolloClient({
   uri: "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 export default apolloClient;
