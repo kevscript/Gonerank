11727768e04d5927ffa947abe31a30f22d49608b
kevscript <kevscripted@gmail.com>
2022-07-10 20:00:02 +0200

    created basic SelectInput

---
+++ __tests__/SelectInput.test.tsx
@@ -0,0 +1,129 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import SelectInput, { SelectInputProps } from "@/components/shared/SelectInput";
+
+jest.mock("react-hook-form", () => ({
+  ...jest.requireActual("react-hook-form"),
+  useFormContext: () => ({
+    handleSubmit: () => jest.fn(),
+    getValues: () => jest.fn(),
+  }),
+}));
+
+describe("SelectInput", () => {
+  it("displays correct label", () => {
+    const props: SelectInputProps<{ color: "red" }> = {
+      label: "Color",
+      name: "color",
+      error: undefined,
+      value: "red",
+      options: { required: "required" },
+      register: jest.fn(),
+      children: (
+        <>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </>
+      ),
+    };
+
+    render(<SelectInput {...props} />);
+
+    const label = screen.getByLabelText(/color/i);
+    expect(label).toBeInTheDocument();
+  });
+
+  it("renders all options", () => {
+    const props: SelectInputProps<{ color: "red" }> = {
+      label: "Color",
+      name: "color",
+      error: undefined,
+      value: "red",
+      options: { required: "required" },
+      register: jest.fn(),
+      children: (
+        <>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </>
+      ),
+    };
+
+    render(<SelectInput {...props} />);
+
+    const option1 = screen.getByText("Red");
+    const option2 = screen.getByText("Blue");
+    expect(option1).toBeInTheDocument();
+    expect(option2).toBeInTheDocument();
+  });
+
+  it("displays the right value", () => {
+    const props: SelectInputProps<{ color: "red" }> = {
+      label: "Color",
+      name: "color",
+      error: undefined,
+      value: "red",
+      options: { required: "required" },
+      register: jest.fn(),
+      children: (
+        <>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </>
+      ),
+    };
+
+    render(<SelectInput {...props} />);
+
+    const select = screen.getByRole("combobox");
+    expect(select).toHaveDisplayValue("Red");
+  });
+
+  it("displays the right value", () => {
+    const props: SelectInputProps<{ color: "red" }> = {
+      label: "Color",
+      name: "color",
+      error: { message: "Hello", type: "max" },
+      value: "red",
+      options: { required: "required" },
+      register: jest.fn(),
+      children: (
+        <>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </>
+      ),
+    };
+
+    render(<SelectInput {...props} />);
+
+    const select = screen.getByText("Hello");
+    expect(select).toBeInTheDocument();
+  });
+
+  it("simulates selection correctly", async () => {
+    const props: SelectInputProps<{ color: "red" }> = {
+      label: "Color",
+      name: "color",
+      error: undefined,
+      value: "red",
+      options: { required: "required" },
+      register: jest.fn(),
+      children: (
+        <>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </>
+      ),
+    };
+
+    render(<SelectInput {...props} />);
+
+    const select = screen.getByRole("combobox");
+
+    fireEvent.change(select, { target: { value: "blue" } });
+
+    await waitFor(() => {
+      expect(select).toHaveDisplayValue("Blue");
+    });
+  });
+});
---
+++ components/shared/SelectInput.tsx
@@ -0,0 +1,60 @@
+import { ReactNode } from "react";
+import {
+  FieldError,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
+
+export type SelectInputProps<FieldValues> = {
+  label: string;
+  name: Path<FieldValues>;
+  register: UseFormRegister<FieldValues>;
+  error: FieldError | undefined;
+  value: string | Date | number | boolean;
+  options?: RegisterOptions;
+  children: ReactNode;
+  containerStyle?: string;
+};
+
+const SelectInput = <T,>({
+  register,
+  label,
+  error,
+  value,
+  options,
+  name,
+  children,
+  containerStyle,
+}: SelectInputProps<T>) => {
+  return (
+    <label
+      className={`flex flex-col min-w-0 ${
+        containerStyle ? containerStyle : "w-full flex-1"
+      }`}
+    >
+      <span className="ml-2 text-sm">{label}</span>
+      <select
+        {...register(name, options)}
+        className={`h-10 px-2 bg-white border text-base rounded mt-1 ${
+          error
+            ? "border-red-400 outline-red-600"
+            : value
+            ? "border-marine-400 outline-marine-600 bg-marine-50"
+            : "border-gray-200 outline-marine-600"
+        }`}
+      >
+        {children}
+      </select>
+      <div className="min-h-[20px] w-full">
+        {error && (
+          <span className="block text-sm ml-2 text-red-500">
+            {error.message}
+          </span>
+        )}
+      </div>
+    </label>
+  );
+};
+
+export default SelectInput;
