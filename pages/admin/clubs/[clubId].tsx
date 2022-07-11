8cb7748485613441539415b82bfe707ffbe18079
kevscript <kevscripted@gmail.com>
2022-07-11 20:11:39 +0200

    updated Admin Club flow + integrated ClubForm

--- pages/admin/clubs/[clubId].tsx
+++ pages/admin/clubs/[clubId].tsx
@@ -1,15 +1,13 @@
+import ClubForm, { ClubFormInput } from "@/components/forms/ClubForm";
 import ShieldIcon from "@/components/Icons/Shield";
-import Input from "@/components/shared/Input";
 import { NextCustomPage } from "@/pages/_app";
 import { Club } from "@prisma/client";
 import {
   useGetClubsQuery,
   useUpdateClubMutation,
 } from "graphql/generated/queryTypes";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
 
 export type EditClubFormInput = {
   name: string;
@@ -24,22 +22,11 @@ const AdminClubEditPage: NextCustomPage = () => {
   const router = useRouter();
   const { clubId } = router.query;
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    getValues,
-    reset,
-    formState: { errors, isDirty, isValid },
-  } = useForm<EditClubFormInput>({
-    mode: "all",
-  });
-
   const [updateClub] = useUpdateClubMutation({
     onCompleted: () => router.push("/admin/clubs"),
   });
 
-  const handleUpdateClub: SubmitHandler<EditClubFormInput> = (data) => {
+  const handleUpdateClub = (data: ClubFormInput) => {
     club && updateClub({ variables: { id: club.id, data: data } });
   };
 
@@ -47,17 +34,6 @@ const AdminClubEditPage: NextCustomPage = () => {
     const currClub = data?.clubs.find((c) => c.id === clubId);
     currClub && setClub(currClub);
   }, [data, clubId]);
-
-  useEffect(() => {
-    if (club) {
-      reset({
-        name: club.name,
-        abbreviation: club.abbreviation,
-        primary: club.primary,
-        secondary: club.secondary,
-      });
-    }
-  }, [club, reset]);
 
   return (
     <div>
@@ -71,72 +47,15 @@ const AdminClubEditPage: NextCustomPage = () => {
       {error && <div>{error.message}</div>}
       {club && (
         <div className="p-4">
-          <form
-            onSubmit={handleSubmit(handleUpdateClub)}
-            className="flex flex-col w-full"
-          >
-            <div className="flex gap-x-4 mt-4">
-              <Input
-                name="name"
-                label="Name *"
-                register={register}
-                error={errors.name}
-                options={{
-                  required: "requis",
-                }}
-                value={getValues("name")}
-              />
-              <Input
-                name="abbreviation"
-                label="Abbr *"
-                register={register}
-                error={errors.abbreviation}
-                options={{
-                  required: "requis",
-                }}
-                value={getValues("abbreviation")}
-                containerStyle="w-32"
-              />
-            </div>
-            <div className="flex gap-x-4 mt-4">
-              <Input
-                name="primary"
-                label="Primary *"
-                register={register}
-                error={errors.primary}
-                options={{
-                  required: "requis",
-                }}
-                value={getValues("primary")}
-              />
-              <Input
-                name="secondary"
-                label="Secondary *"
-                register={register}
-                error={errors.secondary}
-                options={{
-                  required: "requis",
-                }}
-                value={getValues("secondary")}
-              />
-            </div>
-
-            <div className="flex gap-4 mt-8">
-              <Link href="/admin/clubs" passHref>
-                <button type="button" className="px-2 py-1 bg-gray-200 rounded">
-                  Annuler
-                </button>
-              </Link>
-
-              <button
-                type="submit"
-                className="px-2 py-1 bg-gray-200 rounded"
-                disabled={!isDirty || !isValid}
-              >
-                Editer
-              </button>
-            </div>
-          </form>
+          <ClubForm
+            onSubmit={handleUpdateClub}
+            defaultValues={{
+              name: club.name,
+              abbreviation: club.abbreviation,
+              primary: club.primary,
+              secondary: club.secondary,
+            }}
+          />
         </div>
       )}
     </div>
--- pages/admin/clubs/create.tsx
+++ pages/admin/clubs/create.tsx
@@ -1,10 +1,8 @@
+import ClubForm, { ClubFormInput } from "@/components/forms/ClubForm";
 import ShieldIcon from "@/components/Icons/Shield";
-import Input from "@/components/shared/Input";
 import { NextCustomPage } from "@/pages/_app";
 import { useCreateClubMutation } from "graphql/generated/queryTypes";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { SubmitHandler, useForm } from "react-hook-form";
 
 export type CreateClubFormInput = {
   name: string;
@@ -14,22 +12,16 @@ export type CreateClubFormInput = {
 };
 
 const AdminCreateClubPage: NextCustomPage = () => {
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    formState: { errors, isDirty, isValid },
-  } = useForm<CreateClubFormInput>({ mode: "all" });
-
   const router = useRouter();
 
-  const handleClubCreation: SubmitHandler<CreateClubFormInput> = (data) => {
+  const handleClubCreation = (data: ClubFormInput) => {
     createClub({ variables: { data: data } });
   };
 
   const [createClub] = useCreateClubMutation({
     onCompleted: () => router.push("/admin/clubs"),
     refetchQueries: ["GetClubs"],
+    awaitRefetchQueries: true,
   });
 
   return (
@@ -41,72 +33,7 @@ const AdminCreateClubPage: NextCustomPage = () => {
         <h3>Nouveau Club</h3>
       </div>
       <div className="p-4">
-        <form
-          className="flex flex-col w-full"
-          onSubmit={handleSubmit(handleClubCreation)}
-        >
-          <div className="flex gap-x-4 mt-4">
-            <Input
-              name="name"
-              label="Name *"
-              register={register}
-              error={errors.name}
-              options={{
-                required: "requis",
-              }}
-              value={getValues("name")}
-            />
-            <Input
-              name="abbreviation"
-              label="Abbr *"
-              register={register}
-              error={errors.abbreviation}
-              options={{
-                required: "requis",
-              }}
-              value={getValues("abbreviation")}
-              containerStyle="w-32"
-            />
-          </div>
-          <div className="flex gap-x-4 mt-4">
-            <Input
-              name="primary"
-              label="Primary *"
-              register={register}
-              error={errors.primary}
-              options={{
-                required: "requis",
-              }}
-              value={getValues("primary")}
-            />
-            <Input
-              name="secondary"
-              label="Secondary *"
-              register={register}
-              error={errors.secondary}
-              options={{
-                required: "requis",
-              }}
-              value={getValues("secondary")}
-            />
-          </div>
-
-          <div className="flex gap-x-4 mt-4">
-            <Link href="/admin/clubs" passHref>
-              <button type="button" className="px-2 py-1 bg-gray-200 rounded">
-                Annuler
-              </button>
-            </Link>
-
-            <button
-              type="submit"
-              className="px-2 py-1 bg-gray-200 rounded"
-              disabled={!isDirty || !isValid}
-            >
-              Créer
-            </button>
-          </div>
-        </form>
+        <ClubForm onSubmit={handleClubCreation} />
       </div>
     </>
   );
