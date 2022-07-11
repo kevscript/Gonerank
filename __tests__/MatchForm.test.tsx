06ee0f9373d18d4841a1be3990b1318851fda759
kevscript <kevscripted@gmail.com>
2022-07-11 15:03:00 +0200

    created and tested MatchForm

---
+++ __tests__/MatchForm.test.tsx
@@ -0,0 +1,185 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import MatchForm, { MatchFormProps } from "@/components/forms/MatchForm";
+
+const mockProps: Partial<MatchFormProps> = {
+  seasons: [
+    { id: "1", startDate: new Date(1990, 5, 5) },
+    { id: "2", startDate: new Date(1991, 6, 6) },
+  ],
+  competitions: [
+    { id: "1", name: "Ligue 1", abbreviation: "L1" },
+    { id: "2", name: "Champions League", abbreviation: "C1" },
+  ],
+  clubs: [
+    {
+      id: "1",
+      name: "Saint-Etienne",
+      abbreviation: "ASSE",
+      primary: "#333",
+      secondary: "#444",
+    },
+    {
+      id: "2",
+      name: "Nantes",
+      abbreviation: "FCN",
+      primary: "#555",
+      secondary: "#666",
+    },
+  ],
+};
+
+describe("MatchForm", () => {
+  it("renders correct fields", () => {
+    const mockSubmit = jest.fn();
+    const props: MatchFormProps = {
+      seasons: mockProps.seasons,
+      competitions: mockProps.competitions,
+      clubs: mockProps.clubs,
+      onSubmit: mockSubmit,
+    };
+
+    render(<MatchForm {...props} />);
+
+    expect(screen.getByRole("textbox", { name: /date/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("combobox", { name: /season/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("combobox", { name: /competition/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("combobox", { name: /opponent/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("combobox", { name: /location/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("spinbutton", { name: /score/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("spinbutton", { name: /conceed/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText(/create/i)).toBeInTheDocument();
+    expect(screen.getByText(/cancel/i)).toBeInTheDocument();
+  });
+
+  it("renders correct errors", async () => {
+    const mockSubmit = jest.fn();
+    const props: MatchFormProps = {
+      seasons: mockProps.seasons,
+      competitions: mockProps.competitions,
+      clubs: mockProps.clubs,
+      onSubmit: mockSubmit,
+    };
+
+    render(<MatchForm {...props} />);
+
+    const submitButton = screen.getByText(/create/i);
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const dateError = screen.queryByTestId("error-date");
+      expect(dateError).toBeInTheDocument();
+
+      const seasonError = screen.queryByTestId("error-seasonId");
+      expect(seasonError).not.toBeInTheDocument();
+
+      const competitionError = screen.queryByTestId("error-competitionId");
+      expect(competitionError).not.toBeInTheDocument();
+
+      const clubError = screen.queryByTestId("error-clubId");
+      expect(clubError).not.toBeInTheDocument();
+
+      const locationError = screen.queryByTestId("error-home");
+      expect(locationError).not.toBeInTheDocument();
+
+      const scoredError = screen.queryByTestId("error-scored");
+      expect(scoredError).toBeInTheDocument();
+
+      const conceededError = screen.queryByTestId("error-conceeded");
+      expect(conceededError).toBeInTheDocument();
+    });
+  });
+
+  it("calls onSubmit with correct data", async () => {
+    const mockSubmit = jest.fn();
+    const props: MatchFormProps = {
+      seasons: mockProps.seasons,
+      competitions: mockProps.competitions,
+      clubs: mockProps.clubs,
+      onSubmit: mockSubmit,
+    };
+
+    render(<MatchForm {...props} />);
+
+    const date = screen.getByRole("textbox", { name: /date/i });
+    fireEvent.change(date, { target: { value: "20/05/1998" } });
+
+    const scored = screen.getByRole("spinbutton", { name: /score/i });
+    fireEvent.change(scored, { target: { value: 2 } });
+
+    const conceeded = screen.getByRole("spinbutton", { name: /conceed/i });
+    fireEvent.change(conceeded, { target: { value: 1 } });
+
+    const submitButton = screen.getByText(/create/i);
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockSubmit).toHaveBeenCalledTimes(1);
+      expect(mockSubmit).toHaveBeenCalledWith(
+        expect.objectContaining({
+          date: new Date(1998, 4, 20),
+          seasonId: "1",
+          competitionId: "1",
+          opponentId: "1",
+          home: "home",
+          scored: 2,
+          conceeded: 1,
+        })
+      );
+    });
+  });
+
+  it("renders with default values in props", async () => {
+    const mockSubmit = jest.fn();
+    const props: MatchFormProps = {
+      seasons: mockProps.seasons,
+      competitions: mockProps.competitions,
+      clubs: mockProps.clubs,
+      onSubmit: mockSubmit,
+      defaultValues: {
+        date: new Date("2011-10-05T00:00:00.000Z"),
+        seasonId: "2",
+        competitionId: "2",
+        opponentId: "2",
+        home: "away",
+        scored: 0,
+        conceeded: 3,
+      },
+    };
+
+    render(<MatchForm {...props} />);
+
+    expect(screen.getByRole("textbox", { name: /date/i })).toHaveDisplayValue(
+      "05/10/2011"
+    );
+    expect(
+      screen.getByRole("combobox", { name: /season/i })
+    ).toHaveDisplayValue("1991/1992");
+    expect(
+      screen.getByRole("combobox", { name: /competition/i })
+    ).toHaveDisplayValue("Champions League");
+    expect(
+      screen.getByRole("combobox", { name: /opponent/i })
+    ).toHaveDisplayValue("Nantes");
+    expect(
+      screen.getByRole("combobox", { name: /location/i })
+    ).toHaveDisplayValue("Away");
+    expect(
+      screen.getByRole("spinbutton", { name: /score/i })
+    ).toHaveDisplayValue("0");
+    expect(
+      screen.getByRole("spinbutton", { name: /conceed/i })
+    ).toHaveDisplayValue("3");
+  });
+});
---
+++ components/forms/MatchForm.tsx
@@ -0,0 +1,183 @@
+import {
+  GetClubsQuery,
+  GetCompetitionsQuery,
+  GetSeasonsQuery,
+} from "graphql/generated/queryTypes";
+import Link from "next/link";
+import { useEffect, useMemo } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import DateInput from "../shared/DateInput";
+import Input from "../shared/Input";
+import SelectInput from "../shared/SelectInput";
+
+export type MatchFormProps = {
+  seasons: GetSeasonsQuery["seasons"] | undefined;
+  competitions: GetCompetitionsQuery["competitions"] | undefined;
+  clubs: GetClubsQuery["clubs"] | undefined;
+  onSubmit: (x: MatchFormInput) => unknown;
+  defaultValues?: MatchFormInput;
+};
+
+export type MatchFormInput = {
+  date: Date;
+  seasonId: string;
+  competitionId: string;
+  opponentId: string;
+  home: "home" | "away";
+  scored: number;
+  conceeded: number;
+};
+
+const MatchForm = ({
+  seasons,
+  competitions,
+  clubs,
+  defaultValues,
+  onSubmit,
+}: MatchFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm<MatchFormInput>({
+    mode: "onSubmit",
+    defaultValues: useMemo(() => defaultValues, [defaultValues]),
+  });
+
+  const submitHandler: SubmitHandler<MatchFormInput> = (data) => {
+    onSubmit(data);
+  };
+
+  useEffect(() => {
+    if (!defaultValues && seasons && competitions && clubs) {
+      setValue("seasonId", seasons[0].id);
+      setValue("competitionId", competitions[0].id);
+      setValue("opponentId", clubs[0].id);
+    }
+  }, [clubs, competitions, defaultValues, seasons, setValue]);
+
+  return (
+    <form className="w-full" onSubmit={handleSubmit(submitHandler)}>
+      <div className="w-full flex gap-x-4">
+        <DateInput<MatchFormInput>
+          label="Date"
+          control={control}
+          error={errors.date}
+          name="date"
+          value={getValues("date")}
+          rules={{ required: "required" }}
+        />
+
+        <SelectInput<MatchFormInput>
+          label="Season"
+          name="seasonId"
+          error={errors.seasonId}
+          options={{ required: "requis" }}
+          register={register}
+          value={getValues("seasonId")}
+        >
+          <>
+            {seasons?.map((s) => (
+              <option key={s.id} value={s.id}>
+                {new Date(s.startDate).getFullYear() +
+                  "/" +
+                  (new Date(s.startDate).getFullYear() + 1)}
+              </option>
+            ))}
+          </>
+        </SelectInput>
+      </div>
+
+      <SelectInput<MatchFormInput>
+        label="Competition"
+        name="competitionId"
+        error={errors.competitionId}
+        options={{ required: "required" }}
+        register={register}
+        value={getValues("competitionId")}
+      >
+        <>
+          {competitions?.map((c) => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
+          ))}
+        </>
+      </SelectInput>
+
+      <SelectInput<MatchFormInput>
+        label="Opponent"
+        name="opponentId"
+        error={errors.opponentId}
+        options={{ required: "required" }}
+        register={register}
+        value={getValues("opponentId")}
+      >
+        <>
+          {clubs?.map((c) => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
+          ))}
+        </>
+      </SelectInput>
+
+      <div className="w-full flex gap-x-4">
+        <SelectInput<MatchFormInput>
+          label="Location"
+          name="home"
+          error={errors.home}
+          register={register}
+          value={getValues("home")}
+          containerStyle="flex-1"
+        >
+          <option value={"home"}>Home</option>
+          <option value={"away"}>Away</option>
+        </SelectInput>
+
+        <Input
+          name="scored"
+          type="number"
+          label="Scored"
+          register={register}
+          error={errors.scored}
+          options={{
+            required: "required",
+            valueAsNumber: true,
+            min: { value: 0, message: "min. 0" },
+          }}
+          value={getValues("scored")}
+          containerStyle="w-20"
+        />
+
+        <Input
+          name="conceeded"
+          type="number"
+          label="Conceed"
+          register={register}
+          error={errors.conceeded}
+          options={{
+            required: "required",
+            valueAsNumber: true,
+            min: { value: 0, message: "min. 0" },
+          }}
+          value={getValues("conceeded")}
+          containerStyle="w-20"
+        />
+      </div>
+
+      <div className="flex gap-x-4 mt-8">
+        <Link href="/admin/matches" passHref>
+          <button type="button">Cancel</button>
+        </Link>
+
+        <button type="submit">Create</button>
+      </div>
+    </form>
+  );
+};
+
+export default MatchForm;
