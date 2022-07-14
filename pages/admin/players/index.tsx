ce729c29ea92874e2e95932ac198b8523911bf85
kevscript <kevscripted@gmail.com>
2022-07-14 19:42:59 +0200

    updated Players Table columns

--- pages/admin/players/index.tsx
+++ pages/admin/players/index.tsx
@@ -1,6 +1,11 @@
+import EditIcon from "@/components/Icons/Edit";
+import EyeIcon from "@/components/Icons/Eye";
+import EyeClosedIcon from "@/components/Icons/EyeClosedIcon";
+import TrashIcon from "@/components/Icons/Trash";
 import UserIcon from "@/components/Icons/User";
 import AdminTable from "@/components/shared/AdminTable";
 import Draggable from "@/components/shared/Draggable";
+import TableCell from "@/components/shared/TableCell";
 import { NextCustomPage } from "@/pages/_app";
 import { Player } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
@@ -39,14 +44,23 @@ const AdminPlayersPage: NextCustomPage = () => {
 
   const playerColumns: ColumnDef<Player>[] = [
     {
-      header: "player",
+      header: () => {
+        return (
+          <TableCell>
+            <div className="ml-2 relative w-4 h-4 flex justify-center items-center rounded-full overflow-hidden bg-gray-200">
+              <UserIcon />
+            </div>
+            <span className="ml-4 text-sm">Player</span>
+          </TableCell>
+        );
+      },
       id: "player",
       accessorFn: (player) => player.lastName,
       cell: ({ row }) => {
         const { firstName, lastName, image } = row.original || {};
         return (
-          <div className="flex flex-nowrap">
-            <div className="relative w-6 h-6 flex justify-center items-center rounded-full overflow-hidden">
+          <TableCell className="flex flex-nowrap">
+            <div className="relative w-8 h-8 flex justify-center items-center rounded-full overflow-hidden bg-gray-200">
               {image ? (
                 <Image
                   src={image}
@@ -61,71 +75,138 @@ const AdminPlayersPage: NextCustomPage = () => {
             <span className="whitespace-nowrap ml-2">
               {firstName![0] + ". " + lastName}
             </span>
-          </div>
+          </TableCell>
         );
       },
     },
     {
-      header: "birthDate",
+      header: () => {
+        return (
+          <TableCell>
+            <span className="text-sm">birthDate</span>
+          </TableCell>
+        );
+      },
       accessorKey: "birthDate",
       cell: (info) => {
         const birthDate: Date = new Date(info.getValue());
-        return birthDate.toLocaleDateString();
+        return (
+          <TableCell>
+            <span>{birthDate.toLocaleDateString()}</span>
+          </TableCell>
+        );
       },
     },
     {
-      header: "country",
+      header: () => {
+        return (
+          <TableCell>
+            <span className="text-sm">country</span>
+          </TableCell>
+        );
+      },
       accessorKey: "country",
-      cell: (info) => info.getValue(),
+      cell: (info) => {
+        return (
+          <TableCell>
+            <span>{info.getValue()}</span>
+          </TableCell>
+        );
+      },
     },
     {
-      header: "code",
+      header: () => {
+        return (
+          <TableCell>
+            <span className="text-sm">code</span>
+          </TableCell>
+        );
+      },
       accessorKey: "countryCode",
-      cell: (info) => info.getValue(),
+      cell: (info) => {
+        return (
+          <TableCell>
+            <span>{info.getValue()}</span>
+          </TableCell>
+        );
+      },
       maxSize: 80,
     },
     {
-      header: "status",
+      header: () => {
+        return (
+          <TableCell className="justify-center">
+            <span className="text-sm">status</span>
+          </TableCell>
+        );
+      },
       accessorKey: "active",
       cell: ({ row, getValue }) => {
         const active: boolean = getValue();
         const { id } = row.original!;
         return (
-          <div
-            className="w-full h-full bg-cyan-500"
+          <TableCell
+            className={`cursor-pointer justify-center group ${
+              active
+                ? "bg-violet-100 hover:bg-violet-400"
+                : "bg-gray-200 hover:bg-gray-300"
+            }`}
             onClick={() =>
               updatePlayer({
-                variables: { id: id, data: { active: active ? false : true } },
+                variables: {
+                  id: id,
+                  data: { active: active ? false : true },
+                },
               })
             }
           >
-            <span>{active ? "active" : "inactive"}</span>
-          </div>
+            {active ? (
+              <EyeIcon className="w-5 h-5 fill-gray-500 group-hover:fill-white" />
+            ) : (
+              <EyeClosedIcon className="w-5 h-5 fill-gray-500 group-hover:fill-gray-600" />
+            )}
+          </TableCell>
         );
       },
     },
     {
-      header: "edit",
+      header: () => {
+        return (
+          <TableCell className="justify-center">
+            <span className="text-sm">edit</span>
+          </TableCell>
+        );
+      },
       id: "edit",
       cell: ({ row }) => {
         return (
           <Link href={`/admin/players/${row.original!.id}`} passHref>
-            <div className="w-full h-full bg-green-500">Edit</div>
+            <div className="w-full h-full">
+              <TableCell className="bg-marine-100 cursor-pointer justify-center group hover:bg-marine-400">
+                <EditIcon className="w-4 h-4 fill-black group-hover:fill-white" />
+              </TableCell>
+            </div>
           </Link>
         );
       },
     },
     {
-      header: "delete",
+      header: () => {
+        return (
+          <TableCell className="justify-center">
+            <span className="text-sm">delete</span>
+          </TableCell>
+        );
+      },
       id: "delete",
       cell: ({ row }) => {
         return (
-          <div
-            className="w-full h-full bg-red-500"
+          <TableCell
+            className="bg-red-100 cursor-pointer justify-center group hover:bg-red-400"
             onClick={() => handlePlayerDelete(row.original!.id)}
           >
-            Delete
-          </div>
+            <TrashIcon className="w-4 h-4 fill-black group-hover:fill-white" />
+          </TableCell>
         );
       },
     },
@@ -138,17 +219,19 @@ const AdminPlayersPage: NextCustomPage = () => {
           <a className="px-2 py-1 bg-gray-200 rounded">Ajouter</a>
         </Link>
       </div>
-      {loading && <div>Loading...</div>}
-      {error && <div className="text-red-600">{error.message}</div>}
-      {playersData?.players && (
-        <Draggable>
-          <AdminTable
-            columns={playerColumns}
-            data={playersData.players}
-            frozenId="player"
-          />
-        </Draggable>
-      )}
+      <div className="py-4">
+        {loading && <div>Loading...</div>}
+        {error && <div className="text-red-600">{error.message}</div>}
+        {playersData?.players && (
+          <Draggable>
+            <AdminTable
+              columns={playerColumns}
+              data={playersData.players}
+              frozenId="player"
+            />
+          </Draggable>
+        )}
+      </div>
     </div>
   );
 };
