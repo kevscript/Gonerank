e7c0cd40d010b386e6d2b3c3f3f3b789b7e88ba6
kevscript <kevscripted@gmail.com>
2022-07-15 22:11:53 +0200

    added disabled prop to Button

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -3,6 +3,7 @@ export type ButtonProps = {
   type?: "submit" | "button";
   className?: StringConstructor;
   onClick?: () => unknown;
+  disabled?: boolean;
 };
 
 const Button = ({
@@ -10,14 +11,16 @@ const Button = ({
   className,
   onClick,
   type = "button",
+  disabled,
 }: ButtonProps) => {
   return (
     <button
       type={type}
       className={`min-w-[80px] px-4 py-1.5 border border-gray-400 rounded ${
         className || ""
-      }`}
+      } ${disabled && "bg-red-100"}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {label}
     </button>
