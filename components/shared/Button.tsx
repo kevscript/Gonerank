0977a1292893984007aae2671ffc9c357782a641
kevscript <kevscripted@gmail.com>
2022-07-15 14:54:52 +0200

    added optional onClick prop on Button component

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -2,15 +2,22 @@ export type ButtonProps = {
   label: string;
   type?: "submit" | "button";
   className?: StringConstructor;
+  onClick?: () => unknown;
 };
 
-const Button = ({ label, className, type = "button" }: ButtonProps) => {
+const Button = ({
+  label,
+  className,
+  onClick,
+  type = "button",
+}: ButtonProps) => {
   return (
     <button
       type={type}
       className={`min-w-[80px] px-4 py-1.5 border border-gray-400 rounded ${
         className || ""
       }`}
+      onClick={onClick}
     >
       {label}
     </button>
