00d9227f347df4e5918c743af2977ba965aad3b7
kevscript <kevscripted@gmail.com>
2022-07-16 18:06:31 +0200

    updated TableCell props for styling

--- components/shared/TableCell.tsx
+++ components/shared/TableCell.tsx
@@ -3,20 +3,16 @@ import { ReactNode } from "react";
 export type TableCellProps = {
   children: ReactNode;
   className?: string;
-  onClick?: () => unknown;
+  padding?: string;
 };
 
-const TableCell = ({ children, className, onClick }: TableCellProps) => {
-  return (
-    <div
-      className={`w-full h-full px-2 flex items-center ${
-        className ? className : ""
-      }`}
-      onClick={onClick}
-    >
-      {children}
-    </div>
-  );
+const TableCell = ({
+  children,
+  className = "",
+  padding = "px-2",
+}: TableCellProps) => {
+  const styles = `w-full h-full flex items-center ${padding} ${className}`;
+  return <div className={styles}>{children}</div>;
 };
 
 export default TableCell;
