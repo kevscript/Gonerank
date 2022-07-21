ce7bbf3d17079993a1d94b0e787ecd062acea8fc
kevscript <kevscripted@gmail.com>
2022-07-21 16:40:06 +0200

    updated Admin User table ui

--- pages/admin/users/index.tsx
+++ pages/admin/users/index.tsx
@@ -1,9 +1,11 @@
 import UserIcon from "@/components/Icons/User";
 import AdminTable from "@/components/shared/AdminTable";
 import Draggable from "@/components/shared/Draggable";
+import TableCell from "@/components/shared/TableCell";
 import { NextCustomPage } from "@/pages/_app";
 import { User } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { info } from "console";
 import { useGetUsersQuery } from "graphql/generated/queryTypes";
 import Image from "next/image";
 
@@ -12,34 +14,56 @@ const AdminUsersPage: NextCustomPage = () => {
 
   const userColumns: ColumnDef<User>[] = [
     {
-      header: "user",
+      header: () => (
+        <TableCell>
+          <span className="text-sm">user</span>
+        </TableCell>
+      ),
       id: "user",
       cell: ({ row }) => {
         const { image, name } = row.original || {};
         return (
-          <div className="flex items-center">
-            <div className="w-6 h-6 bg-red-600 flex justify-center items-center rounded-full overflow-hidden">
-              {image ? (
-                <Image src={image} alt="user" width={24} height={24} />
-              ) : (
-                <UserIcon className="w-3 h-3 " />
-              )}
-            </div>
+          <TableCell>
+            <div className="flex items-center">
+              <div className="w-6 h-6 bg-red-600 flex justify-center items-center rounded-full overflow-hidden">
+                {image ? (
+                  <Image src={image} alt="user" width={24} height={24} />
+                ) : (
+                  <UserIcon className="w-3 h-3 " />
+                )}
+              </div>
 
-            <span className="ml-2">{name || "user"}</span>
-          </div>
+              <span className="ml-2">{name || "user"}</span>
+            </div>
+          </TableCell>
         );
       },
     },
     {
-      header: "email",
+      header: () => (
+        <TableCell>
+          <span className="text-sm">email</span>
+        </TableCell>
+      ),
       accessorKey: "email",
-      cell: (info) => info.getValue(),
+      cell: (info) => (
+        <TableCell>
+          <span className="text-sm">{info.getValue()}</span>
+        </TableCell>
+      ),
     },
     {
-      header: "role",
+      header: () => (
+        <TableCell>
+          <span>role</span>
+        </TableCell>
+      ),
       accessorKey: "role",
-      cell: (info) => info.getValue(),
+      cell: (info) => (
+        <TableCell>
+          <span>{info.getValue()}</span>
+        </TableCell>
+      ),
     },
   ];
 
