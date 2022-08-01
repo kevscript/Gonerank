83de3b88f8ad28ca7f2d26970d86c9f6f001e57b
kevscript <kevscripted@gmail.com>
2022-08-01 20:48:55 +0200

    updated TableCell: added title prop

--- components/shared/TableCell.tsx
+++ components/shared/TableCell.tsx
@@ -4,15 +4,21 @@ export type TableCellProps = {
   children: ReactNode;
   className?: string;
   padding?: string;
+  title?: string;
 };
 
 const TableCell = ({
   children,
   className = "",
   padding = "px-2",
+  title,
 }: TableCellProps) => {
   const styles = `w-full h-full flex items-center ${padding} ${className}`;
-  return <div className={styles}>{children}</div>;
+  return (
+    <div className={styles} title={title}>
+      {children}
+    </div>
+  );
 };
 
 export default TableCell;
