b79686052913a9977f023b12edb213598cb4380c
kevscript <kevscripted@gmail.com>
2022-08-04 23:08:55 +0200

    added production URL for apollo client URI initialization

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: `${
+    process.env.NODE_ENV === "production"
+      ? "https://gonerank.vercel.app/api/graphql"
+      : "http://localhost:3000/api/graphql"
+  }`,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
