a61d2ed718b8c719974a9d98af6ba8f3e9e33d22
kevscript <kevscripted@gmail.com>
2022-09-01 19:45:26 +0200

    added mobile layout for OptionsFilter

--- components/filters/OptionsFilter.tsx
+++ components/filters/OptionsFilter.tsx
@@ -42,28 +42,79 @@ const OptionsFilter = ({
   isAuth,
 }: OptionsFilterProps) => {
   return (
-    <div className="flex flex-col flex-wrap justify-between gap-2 sm:flex-row lg:gap-y-0">
-      {visual && (
-        <div className="flex flex-row">
-          <VisualFilter
-            toggleVisual={toggleVisual || console.log}
-            visual={visual}
-          />
+    <>
+      {/** Desktop */}
+      <div className="flex-col flex-wrap justify-between hidden gap-2 md:flex sm:flex-row lg:gap-y-0">
+        {visual && (
+          <div className="flex flex-row">
+            <VisualFilter
+              toggleVisual={toggleVisual || console.log}
+              visual={visual}
+            />
+          </div>
+        )}
+
+        <div className="flex flex-col flex-wrap gap-2 sm:flex-row">
+          {isAuth && who && (
+            <WhoFilter toggleWho={toggleWho || console.log} who={who} />
+          )}
+
+          {location && (
+            <LocationFilter
+              location={location}
+              toggleLocation={toggleLocation || console.log}
+            />
+          )}
+
+          {competitions && (
+            <select
+              className="h-10 px-2 text-sm border-2 border-gray-100 rounded outline-none cursor-pointer dark:border-dark-300 text-marine-600 dark:text-white dark:bg-dark-400"
+              value={currentCompetitionId}
+              onChange={handleCompetitionChange}
+            >
+              <option value="all" className="text-black dark:text-white">
+                Toutes compétitions
+              </option>
+              {competitions.map((comp) => (
+                <option
+                  key={comp.id}
+                  value={comp.id}
+                  className="text-black dark:text-white"
+                >
+                  {comp.name}
+                </option>
+              ))}
+            </select>
+          )}
+          {seasons && currentSeasonId && (
+            <SeasonSelector
+              currentSeasonId={currentSeasonId}
+              handleChange={handleSeasonChange!}
+              seasons={seasons}
+            />
+          )}
         </div>
-      )}
+      </div>
 
-      <div className="flex flex-col flex-wrap gap-2 sm:flex-row">
-        {isAuth && who && (
-          <WhoFilter toggleWho={toggleWho || console.log} who={who} />
-        )}
-
-        {location && (
-          <LocationFilter
-            location={location}
-            toggleLocation={toggleLocation || console.log}
-          />
-        )}
-
+      {/** Mobile/Tablet */}
+      <div className="flex flex-col w-full gap-2 md:hidden">
+        <div className="flex flex-row justify-between w-full gap-2">
+          {visual && (
+            <VisualFilter
+              toggleVisual={toggleVisual || console.log}
+              visual={visual}
+            />
+          )}
+          {isAuth && who && (
+            <WhoFilter toggleWho={toggleWho || console.log} who={who} />
+          )}
+          {location && (
+            <LocationFilter
+              location={location}
+              toggleLocation={toggleLocation || console.log}
+            />
+          )}
+        </div>
         {competitions && (
           <select
             className="h-10 px-2 text-sm border-2 border-gray-100 rounded outline-none cursor-pointer dark:border-dark-300 text-marine-600 dark:text-white dark:bg-dark-400"
@@ -92,7 +143,7 @@ const OptionsFilter = ({
           />
         )}
       </div>
-    </div>
+    </>
   );
 };
 
