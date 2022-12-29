0745f691cc68a294461769ab3662948b587c2f63
kevscript <kevscripted@gmail.com>
2022-12-29 20:55:05 +0100

    sorted and styled list of players in match vote components

--- components/MatchDisplay/MatchInfo.tsx
+++ components/MatchDisplay/MatchInfo.tsx
@@ -1,3 +1,4 @@
+import { percentageToColor } from "@/utils/percentageToColor";
 import {
   GetDisplayMatchQuery,
   GetRatingsQuery,
@@ -13,51 +14,54 @@ const MatchInfo = ({ match, userRatings }: MatchInfoProps) => {
   return (
     <>
       <ul className="flex flex-col grid-cols-2 gap-2 mt-4 md:grid xl:mt-8 ">
-        {match?.stats.map((player) => (
-          <li
-            key={player.playerId}
-            className="flex items-center justify-between w-full h-10 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-400 md:h-12 xl:h-16 xl:border-none first:mt-0 xl:drop-shadow-sm"
-          >
-            <div className="flex items-center flex-1 w-full">
-              <div className="relative flex items-center justify-center w-6 h-6 ml-2 overflow-hidden bg-gray-300 rounded-full dark:bg-dark-600 md:w-8 md:h-8 xl:w-12 xl:h-12">
-                {player.image ? (
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${player.image}`}
-                    layout="fill"
-                    objectFit="cover"
-                    alt={`${player.firstName} ${player.lastName}`}
-                  />
-                ) : null}
-              </div>
-              <span className="ml-2 text-sm whitespace-nowrap md:text-base md:font-medium">
-                {player.firstName![0] + ". " + player.lastName}
-              </span>
-            </div>
-
-            {userRatings && (
-              <div
-                className="flex items-center justify-center w-10 h-full text-sm font-bold md:text-base md:w-12 xl:w-14 bg-marine-100 dark:bg-dark-400"
-                title="la note de l'utilisateur"
+        {match &&
+          [...match.stats]
+            .sort((a, b) => (a.lastName > b.lastName ? 1 : -1))
+            .map((player) => (
+              <li
+                key={player.playerId}
+                className="flex items-center justify-between w-full h-10 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-400 md:h-12 xl:h-16 xl:border-none first:mt-0 xl:drop-shadow-sm"
               >
-                <span className="text-marine-600 dark:text-marine-400 font-num xl:font-bold">
-                  {userRatings.find((r) => r.playerId === player.playerId)
-                    ?.rating || "-"}
-                </span>
-              </div>
-            )}
+                <div className="flex items-center flex-1 w-full">
+                  <div className="relative flex items-center justify-center w-6 h-6 ml-2 overflow-hidden bg-gray-300 rounded-full dark:bg-dark-600 md:w-8 md:h-8 xl:w-12 xl:h-12">
+                    {player.image ? (
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${player.image}`}
+                        layout="fill"
+                        objectFit="cover"
+                        alt={`${player.firstName} ${player.lastName}`}
+                      />
+                    ) : null}
+                  </div>
+                  <span className="ml-2 text-sm whitespace-nowrap md:text-base md:font-medium">
+                    {player.firstName![0] + ". " + player.lastName}
+                  </span>
+                </div>
 
-            <div
-              title="moyenne des notes de la communauté"
-              className={`flex h-full items-center w-12 xl:w-14 justify-center font-num text-sm font-bold md:text-base bg-marine-200 dark:bg-dark-500 text-marine-800 dark:text-white`}
-            >
-              <span>
-                {player.avgSum
-                  ? (player.avgSum / player.numOfAvg).toFixed(2)
-                  : "-"}
-              </span>
-            </div>
-          </li>
-        ))}
+                {userRatings && (
+                  <div
+                    className="flex items-center justify-center w-10 h-full text-sm font-bold md:text-base md:w-12 xl:w-14 bg-marine-100 dark:bg-dark-400"
+                    title="la note de l'utilisateur"
+                  >
+                    <span className="text-marine-600 dark:text-marine-400 font-num xl:font-bold">
+                      {userRatings.find((r) => r.playerId === player.playerId)
+                        ?.rating || "-"}
+                    </span>
+                  </div>
+                )}
+
+                <div
+                  title="moyenne des notes de la communauté"
+                  className={`flex h-full items-center w-12 xl:w-14 justify-center font-num text-sm font-bold md:text-base bg-marine-200 dark:bg-dark-500 text-marine-800 dark:text-white`}
+                >
+                  <span>
+                    {player.avgSum
+                      ? (player.avgSum / player.numOfAvg).toFixed(2)
+                      : "-"}
+                  </span>
+                </div>
+              </li>
+            ))}
       </ul>
     </>
   );
--- components/MatchDisplay/MatchVoter.tsx
+++ components/MatchDisplay/MatchVoter.tsx
@@ -1,3 +1,4 @@
+import { percentageToColor } from "@/utils/percentageToColor";
 import { GetDisplayMatchQuery } from "graphql/generated/queryTypes";
 import Image from "next/image";
 import { useMemo, useState } from "react";
@@ -52,61 +53,64 @@ const MatchVoter = ({ match, onSubmit }: MatchVoterProps) => {
     <>
       <form className="mt-4 lg:mt-8">
         <ul className="flex flex-col grid-cols-2 gap-2 md:grid">
-          {match?.stats.map((player) => (
-            <li
-              key={player.playerId}
-              className="flex items-center justify-between w-full h-10 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-400 md:h-12 xl:h-16 xl:border-none xl:drop-shadow-sm first:mt-0"
-            >
-              <div className="flex items-center flex-1 w-full">
-                <div className="relative flex items-center justify-center w-6 h-6 ml-2 overflow-hidden bg-gray-300 rounded-full dark:bg-dark-600 md:w-8 md:h-8 xl:w-12 xl:h-12">
-                  {player.image ? (
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${player.image}`}
-                      layout="fill"
-                      objectFit="cover"
-                      alt={`${player.firstName} ${player.lastName}`}
-                    />
-                  ) : null}
-                </div>
-                <span className="ml-2 text-sm md:text-base md:font-medium whitespace-nowrap">
-                  {player.firstName![0] + ". " + player.lastName}
-                </span>
-              </div>
+          {match &&
+            [...match.stats]
+              .sort((a, b) => (a.lastName > b.lastName ? 1 : -1))
+              .map((player) => (
+                <li
+                  key={player.playerId}
+                  className="flex items-center justify-between w-full h-10 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-400 md:h-12 xl:h-16 xl:border-none xl:drop-shadow-sm first:mt-0"
+                >
+                  <div className="flex items-center flex-1 w-full">
+                    <div className="relative flex items-center justify-center w-6 h-6 ml-2 overflow-hidden bg-gray-300 rounded-full dark:bg-dark-600 md:w-8 md:h-8 xl:w-12 xl:h-12">
+                      {player.image ? (
+                        <Image
+                          src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/avatars/${player.image}`}
+                          layout="fill"
+                          objectFit="cover"
+                          alt={`${player.firstName} ${player.lastName}`}
+                        />
+                      ) : null}
+                    </div>
+                    <span className="ml-2 text-sm md:text-base md:font-medium whitespace-nowrap">
+                      {player.firstName![0] + ". " + player.lastName}
+                    </span>
+                  </div>
 
-              <div className="flex items-center h-full">
-                <button
-                  type="button"
-                  className="flex items-center justify-center w-6 h-full font-bold lg:w-8 xl:w-10 bg-marine-100 hover:bg-marine-200 text-marine-600 dark:bg-dark-400 dark:hover:bg-dark-300 dark:text-marine-400"
-                  onClick={() => handleDecrement(player.playerId)}
-                  data-testid={`decrement-${player.playerId}`}
-                >
-                  -
-                </button>
-                <label
-                  className="sr-only"
-                  htmlFor={`${player.playerId}`}
-                >{`${player.firstName} ${player.lastName}`}</label>
-                <input
-                  {...register(`${player.playerId}`, {
-                    min: 1,
-                    max: 10,
-                    valueAsNumber: true,
-                  })}
-                  className={`w-10 xl:w-14 xl:font-black xl:text-lg text-center font-num font-bold outline-none h-full text-marine-600 dark:text-white`}
-                  readOnly={true}
-                  id={`${player.playerId}`}
-                />
-                <button
-                  type="button"
-                  className="flex items-center justify-center w-6 h-full font-bold lg:w-8 xl:w-10 bg-marine-100 hover:bg-marine-200 text-marine-600 dark:bg-dark-400 dark:hover:bg-dark-300 dark:text-marine-400"
-                  onClick={() => handleIncrement(player.playerId)}
-                  data-testid={`increment-${player.playerId}`}
-                >
-                  +
-                </button>
-              </div>
-            </li>
-          ))}
+                  <div className="flex items-center h-full">
+                    <button
+                      type="button"
+                      className="flex items-center justify-center w-6 h-full font-bold lg:w-8 xl:w-10 bg-marine-100 hover:bg-marine-200 text-marine-600 dark:bg-dark-400 dark:hover:bg-dark-300 dark:text-marine-400"
+                      onClick={() => handleDecrement(player.playerId)}
+                      data-testid={`decrement-${player.playerId}`}
+                    >
+                      -
+                    </button>
+                    <label
+                      className="sr-only"
+                      htmlFor={`${player.playerId}`}
+                    >{`${player.firstName} ${player.lastName}`}</label>
+                    <input
+                      {...register(`${player.playerId}`, {
+                        min: 1,
+                        max: 10,
+                        valueAsNumber: true,
+                      })}
+                      className={`w-10 xl:w-14 xl:font-black xl:text-lg text-center font-num font-bold outline-none h-full text-marine-600 dark:text-white`}
+                      readOnly={true}
+                      id={`${player.playerId}`}
+                    />
+                    <button
+                      type="button"
+                      className="flex items-center justify-center w-6 h-full font-bold lg:w-8 xl:w-10 bg-marine-100 hover:bg-marine-200 text-marine-600 dark:bg-dark-400 dark:hover:bg-dark-300 dark:text-marine-400"
+                      onClick={() => handleIncrement(player.playerId)}
+                      data-testid={`increment-${player.playerId}`}
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+              ))}
         </ul>
         <div className="flex justify-center mt-4 xl:mt-8 gap-x-4">
           <Button
@@ -126,7 +130,31 @@ const MatchVoter = ({ match, onSubmit }: MatchVoterProps) => {
             Veuillez confirmer vos notes, vous ne pourrez plus les modifier :
           </p>
           <ul className="flex flex-wrap gap-1 mt-4 lg:gap-2">
-            {match?.stats.map((player) => (
+            {Object.entries(getValues())
+              .sort((a, b) => (a[1] > b[1] ? -1 : 1))
+              .map((item) => (
+                <li
+                  key={item[0]}
+                  className="flex items-center justify-between w-48 h-8 overflow-hidden text-sm border rounded whitespace-nowrap bg-marine-50 border-marine-200 dark:bg-dark-300 dark:border-marine-600"
+                >
+                  <span className="flex-1 ml-2">
+                    {match?.stats.find((p) => p.playerId === item[0])!
+                      .firstName[0] +
+                      ". " +
+                      match?.stats.find((p) => p.playerId === item[0])!
+                        .lastName}
+                  </span>
+                  <span
+                    className="flex items-center justify-center w-10 h-full font-bold text-white font-num"
+                    style={{
+                      backgroundColor: `${percentageToColor(item[1] / 10)}`,
+                    }}
+                  >
+                    {item[1]}
+                  </span>
+                </li>
+              ))}
+            {/* {match?.stats.map((player) => (
               <li
                 key={player.playerId}
                 className="flex items-center justify-between w-48 h-8 overflow-hidden text-sm border rounded whitespace-nowrap bg-marine-50 border-marine-200 dark:bg-dark-300 dark:border-marine-600"
@@ -138,7 +166,7 @@ const MatchVoter = ({ match, onSubmit }: MatchVoterProps) => {
                   {getValues(player.playerId)}
                 </span>
               </li>
-            ))}
+            ))} */}
           </ul>
           <div className="flex justify-end mt-8 gap-x-2">
             <Button
