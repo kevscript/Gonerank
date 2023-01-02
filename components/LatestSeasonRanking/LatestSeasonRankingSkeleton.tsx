7c24c43ff7da871569402f84cf05cf9ed6de3127
kevscript <kevscripted@gmail.com>
2023-01-02 17:42:30 +0100

    fixed UI LatestSeasonRankingSkeleton

--- components/LatestSeasonRanking/LatestSeasonRankingSkeleton.tsx
+++ components/LatestSeasonRanking/LatestSeasonRankingSkeleton.tsx
@@ -3,17 +3,17 @@ import Spinner from "../shared/Spinner";
 const LatestSeasonRankingSkeleton = () => {
   return (
     <div className="relative flex-col hidden lg:flex lg:w-60 2xl:w-72">
-      <div className="flex flex-col flex-1 p-4 overflow-hidden bg-marine-50 dark:bg-dark-500">
-        <div className="w-full h-24 bg-white/70 dark:bg-dark-400 drop-shadow-sm"></div>
+      <div className="flex flex-col flex-1 p-4 overflow-hidden bg-marine-50 dark:bg-dark-500 rouned-sm">
+        <div className="w-full h-24 rounded-sm bg-white/70 dark:bg-dark-400 drop-shadow-sm"></div>
         <ul className="flex flex-col flex-1 flex-shrink-0 w-full gap-1 my-4 overflow-hidden">
           {[...Array(17)].map((n, i) => (
             <li
               key={i}
-              className="flex-shrink-0 w-full h-10 bg-white/70 dark:bg-dark-400 drop-shadow-sm rounde-sm"
+              className="flex-shrink-0 w-full h-10 rounded-sm bg-white/70 dark:bg-dark-400 drop-shadow-sm"
             ></li>
           ))}
         </ul>
-        <div className="absolute -translate-x-1/2 left-1/2 top-1/2 ">
+        <div className="absolute z-10 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 ">
           <Spinner />
         </div>
       </div>
