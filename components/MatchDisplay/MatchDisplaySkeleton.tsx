c5d1c9df7c8ac6ddde341d547fe829888c562c2f
kevscript <kevscripted@gmail.com>
2023-01-02 17:45:33 +0100

    updated UI of MatchDisplaySkeleton

--- components/MatchDisplay/MatchDisplaySkeleton.tsx
+++ components/MatchDisplay/MatchDisplaySkeleton.tsx
@@ -2,10 +2,24 @@ import Spinner from "../shared/Spinner";
 
 const MatchDisplaySkeleton = () => {
   return (
-    <div className="flex flex-col flex-1 min-h-fit gap-y-8">
-      <div className="w-full h-32 bg-gray-200 dark:bg-dark-400"></div>
-      <div className="flex items-center justify-center flex-1 w-full bg-gray-200 dark:bg-dark-400">
+    <div className="relative flex flex-col flex-1 overflow-hidden min-h-fit gap-y-8">
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 z-10">
         <Spinner />
+      </div>
+      <div className="flex items-center justify-between w-full h-32 p-4 rounded-sm bg-marine-50 dark:bg-dark-500 flex-nowrap gap-x-4 drop-shadow-sm">
+        <div className="flex-1 h-24 rounded-sm dark:bg-dark-400 bg-white/70 drop-shadow-sm"></div>
+        <div className="w-24 h-24 rounded-sm dark:bg-dark-400 bg-white/70 drop-shadow-sm"></div>
+        <div className="flex-1 h-24 rounded-sm dark:bg-dark-400 bg-white/70 drop-shadow-sm"></div>
+      </div>
+      <div className="flex flex-1 w-full p-4 rounded-sm bg-marine-50 dark:bg-dark-500">
+        <ul className="grid w-full grid-cols-2 gap-2 overflow-hidden auto-rows-min">
+          {[...Array(14)].map((n, i) => (
+            <li
+              key={i}
+              className="h-24 rounded-sm dark:bg-dark-400 bg-white/70 drop-shadow-sm"
+            ></li>
+          ))}
+        </ul>
       </div>
     </div>
   );
