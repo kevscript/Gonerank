b6bcf3c1081478bf84734f37d4ab28719ff69735
kevscript <kevscripted@gmail.com>
2023-02-19 20:07:14 +0100

    installed visx dependencies and created VisxPlayersAvgLinearCHart

---
+++ components/charts/VisxPlayersAvgLinearChart.tsx
@@ -0,0 +1,296 @@
+import { AxisLeft, AxisBottom } from "@visx/axis";
+import { Group } from "@visx/group";
+import { scaleLinear, scalePoint } from "@visx/scale";
+import { LinePath } from "@visx/shape";
+import * as curves from "@visx/curve";
+import { Grid } from "@visx/grid";
+import { useTooltip, TooltipWithBounds } from "@visx/tooltip";
+
+import { FormattedPlayersChartData } from "@/utils/charts/formatPlayersChartData";
+
+type VisxPlayersAvgLinearChartProps = {
+  players: FormattedPlayersChartData[];
+  idsToShow: string[];
+  highlightedPlayer: string | null;
+  theme: string;
+  dimensions: { width: number; height: number };
+};
+
+type TooltipData = {
+  player: Omit<FormattedPlayersChartData, "matches">;
+  match: FormattedPlayersChartData["matches"][0];
+  color: string;
+};
+
+const defaultDimensions = { width: 600, height: 400 };
+
+const VisxPlayersAvgLinearChart = ({
+  players,
+  idsToShow,
+  dimensions = defaultDimensions,
+  theme,
+}: VisxPlayersAvgLinearChartProps) => {
+  const { width, height } = dimensions;
+
+  const margin = { top: 32, right: 32, bottom: 48, left: 32 };
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
+
+  const xScale = scalePoint({
+    domain: [...players[0].matches.map((match) => match.date)],
+    range: [0, xMax],
+    padding: 0.5,
+  });
+
+  const refScale = scalePoint({
+    domain: [...players[0].matches.map((match) => match.date)],
+    range: [0, xMax],
+  });
+
+  const yScale = scaleLinear<number>({
+    domain: [0, 10],
+    nice: true,
+    range: [yMax, 0],
+  });
+
+  const {
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+    tooltipOpen,
+    showTooltip,
+    hideTooltip,
+  } = useTooltip({ tooltipData: {} as TooltipData });
+
+  const handlePointHover = (
+    e: React.MouseEvent<SVGCircleElement>,
+    data: { playerId: string; matchId: string; color: string }
+  ) => {
+    const hoveredPlayer = players.find((p) => p.id === data.playerId);
+    const hoveredMatch = hoveredPlayer?.matches.find(
+      (m) => m.id === data.matchId
+    );
+    if (hoveredPlayer && hoveredMatch) {
+      const { matches, ...playerInfo } = hoveredPlayer;
+      const info: TooltipData = {
+        player: playerInfo,
+        match: hoveredMatch,
+        color: data.color,
+      };
+
+      showTooltip({
+        tooltipLeft: e.clientX,
+        tooltipTop: e.clientY,
+        tooltipData: info,
+      });
+    }
+  };
+
+  return (
+    <>
+      <svg width={width} height={height}>
+        <Group left={margin.left} top={margin.top}>
+          <AxisLeft
+            scale={yScale}
+            tickValues={[1, 3, 5, 7, 9]}
+            tickLength={4}
+            stroke={theme === "dark" ? "#fafafa" : "#111111"}
+            tickStroke={theme === "dark" ? "#fafafa" : "#111111"}
+            tickLabelProps={() => ({
+              dx: -12,
+              dy: 4,
+              fontSize: 12,
+              fill: theme === "dark" ? "#fafafa" : "#111111",
+            })}
+          />
+          <AxisBottom
+            scale={xScale}
+            top={yMax}
+            tickLength={4}
+            tickFormat={(d) =>
+              new Date(d).toLocaleDateString(undefined, {
+                month: "numeric",
+                day: "numeric",
+              })
+            }
+            stroke={theme === "dark" ? "#fafafa" : "#111111"}
+            tickStroke={theme === "dark" ? "#fafafa" : "#111111"}
+            tickLabelProps={() => ({
+              dx: -16,
+              dy: 6,
+              fill: theme === "dark" ? "#fafafa" : "#111111",
+              fontSize: 12,
+            })}
+          />
+          <Grid
+            xScale={xScale}
+            yScale={yScale}
+            width={xMax}
+            height={yMax}
+            rowTickValues={[1, 3, 5, 7, 9]}
+            numTicksColumns={players[0].matches.length}
+            stroke={theme === "dark" ? "#fafafa" : "#111111"}
+            strokeWidth={0.5}
+            strokeOpacity={theme === "dark" ? 0.25 : 0.1}
+            numTicksRows={0}
+          />
+          <Group>
+            <LinePath
+              data={players[0].matches}
+              x={(d) => refScale(d.date)!}
+              y={yScale(5)}
+              stroke={theme === "dark" ? "#fafafa" : "#111111"}
+              strokeWidth={0.5}
+              strokeOpacity={theme === "dark" ? 1 : 0.5}
+              strokeDasharray={4}
+            />
+          </Group>
+          {players.map((p, i) => {
+            return (
+              <Group key={`line-${p.id}`}>
+                <LinePath
+                  className="pointer-events-none"
+                  defined={(d) => d.averageQuantity !== undefined}
+                  curve={curves["curveMonotoneX"]}
+                  data={p.matches.filter((m) => m.averageQuantity)}
+                  x={(d) => xScale(d.date)!}
+                  y={(d) =>
+                    d.averageQuantity
+                      ? yScale(d.averageSum / d.averageQuantity)
+                      : 0
+                  }
+                  stroke={`hsla(${
+                    (360 / idsToShow.length) * idsToShow.indexOf(p.id) + 1
+                  }, 100%, ${theme === "dark" ? "70%" : "50%"}, ${
+                    idsToShow.length === 0 ||
+                    idsToShow.some((id) => id === p.id)
+                      ? "60%"
+                      : "0%"
+                  })`}
+                  strokeWidth={2}
+                  strokeOpacity={1}
+                />
+                {p.matches.map((m, j) => {
+                  return (
+                    m.averageQuantity && (
+                      <circle
+                        className={`cursor-pointer  hover:stroke-2 ${
+                          theme === "dark"
+                            ? "hover:stroke-white"
+                            : "hover:stroke-black"
+                        }`}
+                        key={`${p.id}__${m.id}`}
+                        r={3}
+                        cx={xScale(m.date)!}
+                        cy={yScale(m.averageSum / m.averageQuantity)}
+                        fill={`hsla(${
+                          (360 / idsToShow.length) * idsToShow.indexOf(p.id) + 1
+                        }, 100%, ${theme === "dark" ? "70%" : "50%"}, ${
+                          idsToShow.length === 0 ||
+                          idsToShow.some((id) => id === p.id)
+                            ? "100%"
+                            : "0%"
+                        })`}
+                        style={{
+                          display:
+                            idsToShow.length === 0 ||
+                            idsToShow.some((id) => id === p.id)
+                              ? "block"
+                              : "none",
+                        }}
+                        onMouseOver={(e) =>
+                          handlePointHover(e, {
+                            playerId: p.id,
+                            matchId: m.id,
+                            color: `hsla(${
+                              (360 / idsToShow.length) *
+                                idsToShow.indexOf(p.id) +
+                              1
+                            },  ${theme === "dark" ? "70%" : "50%"}, ${
+                              idsToShow.length === 0 ||
+                              idsToShow.some((id) => id === p.id)
+                                ? "60%"
+                                : "0%"
+                            })`,
+                          })
+                        }
+                        onMouseOut={hideTooltip}
+                      />
+                    )
+                  );
+                })}
+              </Group>
+            );
+          })}
+        </Group>
+      </svg>
+
+      {tooltipOpen && (
+        <TooltipWithBounds
+          className="!p-0 border border-gray-400 !rounded-sm !overflow-hidden !bg-gray-400"
+          key={tooltipData ? tooltipData.player.id : Math.random()}
+          top={tooltipTop}
+          left={tooltipLeft}
+        >
+          <div className="flex flex-col flex-nowrap gap-y-[1px]">
+            <div className="relative flex items-center justify-between w-full p-2 font-medium text-black bg-white flex-nowrap gap-x-2">
+              <div
+                className="absolute top-0 left-0 w-full h-full"
+                style={{ backgroundColor: tooltipData?.color, opacity: 0.2 }}
+              ></div>
+              <span>
+                {tooltipData?.player.firstName[0] +
+                  ". " +
+                  tooltipData?.player.lastName}
+              </span>
+              <div
+                className="w-2 h-2 rounded-full"
+                style={{ backgroundColor: tooltipData?.color }}
+              ></div>
+            </div>
+            <div className="flex w-full flex-nowrap gap-x-[1px]">
+              <div className="flex flex-col flex-nowrap gap-y-[1px]">
+                <div className="flex justify-between w-full gap-x-[1px]">
+                  <div
+                    className="justify-center flex-1 px-2 py-1 text-xs font-bold text-white"
+                    style={{
+                      backgroundColor: tooltipData?.match.opponent?.primary,
+                    }}
+                  >
+                    {tooltipData?.match.opponent?.abbreviation}
+                  </div>
+                  <div className="justify-center px-2 py-1 text-xs bg-gray-100">
+                    {tooltipData?.match.home ? "H" : "A"}
+                  </div>
+                </div>
+                <div className="flex justify-between w-full gap-x-[1px]">
+                  <div className="justify-center flex-1 px-2 py-1 text-xs bg-gray-100">
+                    {tooltipData?.match.competition?.abbreviation}
+                  </div>
+                  <div className="justify-center px-2 py-1 text-xs bg-gray-100">
+                    {new Date(tooltipData!.match.date).toLocaleDateString(
+                      undefined,
+                      {
+                        month: "numeric",
+                        day: "numeric",
+                      }
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center justify-center min-w-[48px] flex-1 text-base font-bold bg-gray-200 font-num text-black">
+                <span>
+                  {tooltipData?.match.averageQuantity &&
+                    tooltipData?.match.averageSum /
+                      tooltipData?.match.averageQuantity}
+                </span>
+              </div>
+            </div>
+          </div>
+        </TooltipWithBounds>
+      )}
+    </>
+  );
+};
+
+export default VisxPlayersAvgLinearChart;
