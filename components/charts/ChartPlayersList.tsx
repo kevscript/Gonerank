97b4bc26ba565d178e2e798ba116b373ca3afdac
kevscript <kevscripted@gmail.com>
2023-02-27 16:10:30 +0100

    made ChartPlayersList and VisxChartContainer a bit more usable on mobile

--- components/charts/ChartPlayersList.tsx
+++ components/charts/ChartPlayersList.tsx
@@ -15,7 +15,7 @@ const ChartPlayersList = ({
 }: ChartPlayersListProps) => {
   return (
     <div className="relative flex p-[1px] overflow-hidden bg-gray-200 rounded-sm w-72 dark:bg-dark-300">
-      <ul className="flex flex-col w-full overflow-y-scroll rounded-sm">
+      <ul className="flex flex-col w-full overflow-x-hidden overflow-y-auto rounded-sm">
         {players
           .filter((p) => p.matches.length > 0)
           .sort((a, b) => {
@@ -29,7 +29,7 @@ const ChartPlayersList = ({
           .map((player, i) => (
             <li
               key={player.id}
-              className={`h-10 flex flex-shrink-0 items-center px-4 cursor-pointer group border-b-2 border-gray-100 dark:border-dark-300 ${
+              className={`h-10 flex shrink-0 items-center cursor-pointer group border-b-2 border-gray-100 dark:border-dark-300 ${
                 idsToShow.includes(player.id)
                   ? "bg-white hover:bg-gray-100 dark:bg-dark-400  dark:hover:bg-black/25"
                   : "bg-gray-100 hover:bg-white dark:bg-dark-600 dark:hover:bg-dark-400"
@@ -45,7 +45,7 @@ const ChartPlayersList = ({
                 readOnly
               />
               <div
-                className={`w-2 h-2 rounded-full`}
+                className={`w-2 h-2 rounded-full mx-2 md:mx-4 shrink-0`}
                 style={{
                   background: idsToShow.includes(player.id)
                     ? `hsla(${
@@ -58,13 +58,19 @@ const ChartPlayersList = ({
               ></div>
               <label
                 htmlFor={`p-${player.id}`}
-                className={`text-sm ml-4 pointer-events-none ${
+                className={`text-sm pointer-events-none ${
                   idsToShow.includes(player.id)
                     ? `dark:text-white text-black`
                     : "dark:text-gray-300 text-gray-600 group-hover:text-black dark:group-hover:text-white"
                 }`}
               >
-                {player.firstName} {player.lastName}
+                <span className="hidden lg:block">
+                  {`${player.firstName} ${player.lastName}`}
+                </span>
+
+                <span className="block lg:hidden whitespace-nowrap">
+                  {`${player.firstName[0]}. ${player.lastName}`}
+                </span>
               </label>
             </li>
           ))}
--- components/charts/VisxChartContainer.tsx
+++ components/charts/VisxChartContainer.tsx
@@ -11,7 +11,9 @@ const VisxChartContainer = ({ title, children }: VisxChartContainerProps) => {
       <div className="flex items-center px-8 pt-8 pb-4 bg-gray-200 rounded-t-md dark:bg-dark-400">
         <h3 className="text-sm lg:text-base">{title}</h3>
       </div>
-      <div className="p-4"> {children}</div>
+      <div className="flex-shrink-0 w-full overflow-x-auto">
+        <div className="p-4 min-w-[600px]">{children}</div>
+      </div>
     </div>
   );
 };
