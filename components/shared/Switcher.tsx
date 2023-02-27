7f819df1397723f2d2a8495d236f7834cb88888d
kevscript <kevscripted@gmail.com>
2023-02-27 16:37:56 +0100

    updated darkmode switcher

--- components/shared/Switcher.tsx
+++ components/shared/Switcher.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export type SwitcherProps = {
   checked: boolean;
   handleToggle: () => unknown;
@@ -17,12 +19,13 @@ const Switcher = ({ checked, handleToggle, id }: SwitcherProps) => {
         id={id}
         className={`sr-only peer after:checked:translate-x-full checked:after:border-white checked:after:left-1.5`}
       />
-      <div className="absolute w-4 h-4 transition-transform bg-white border-2 border-gray-200 rounded-full peer-checked:translate-x-full peer-checked:border-marine-200"></div>
+      <div className="absolute flex items-center justify-center w-4 h-4 transition-transform bg-yellow-300 border-2 border-yellow-400 rounded-full peer-checked:bg-marine-500 peer-checked:translate-x-full peer-checked:border-marine-600"></div>
+
       <div
         className={`border-2 h-4 w-8 rounded-full ${
           checked
-            ? "bg-marine-600 border-marine-600"
-            : "bg-gray-400 border-gray-400"
+            ? "bg-marine-400 border-marine-600"
+            : "bg-gray-100 border-gray-300"
         }`}
       ></div>
     </label>
