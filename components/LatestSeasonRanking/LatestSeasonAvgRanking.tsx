7f211fd450f6cd00a180c15abd034ca6f0e87fe3
kevscript <kevscripted@gmail.com>
2023-03-01 21:31:09 +0100

    added cursor pointer on LatestSeason rankings

--- components/LatestSeasonRanking/LatestSeasonAvgRanking.tsx
+++ components/LatestSeasonRanking/LatestSeasonAvgRanking.tsx
@@ -46,32 +46,32 @@ const LatestSeasonAvgRanking = ({ stats }: LatestSeasonAvgRankingProps) => {
               <div className="flex items-center justify-center w-8 h-full bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-300 font-num">
                 {i + 1}
               </div>
-              <div className="flex items-center justify-between flex-1 ml-1 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-300">
-                <Link href={`/players/${p.id}`}>
+              <Link href={`/players/${p.id}`}>
+                <div className="flex items-center justify-between flex-1 ml-1 overflow-hidden bg-white border border-gray-100 rounded cursor-pointer  dark:bg-dark-500 dark:border-dark-300">
                   <span className="px-2">
                     {p.firstName[0] + ". " + p.lastName}
                   </span>
-                </Link>
-                <div
-                  className="flex items-center justify-center w-12 h-full bg-marine-100 dark:bg-dark-400 text-marine-600 dark:text-marine-400 font-num"
-                  style={{
-                    color: `${
-                      p.globalAvgQuantity
-                        ? percentageToColor(
-                            (p.globalAvgSum / p.globalAvgQuantity -
-                              minMax.min) /
-                              (minMax.max - minMax.min),
-                            theme as any
-                          )
-                        : "#666666"
-                    }`,
-                  }}
-                >
-                  {p.globalAvgQuantity
-                    ? (p.globalAvgSum / p.globalAvgQuantity).toFixed(2)
-                    : "-"}
+                  <div
+                    className="flex items-center justify-center w-12 h-full bg-marine-100 dark:bg-dark-400 text-marine-600 dark:text-marine-400 font-num"
+                    style={{
+                      color: `${
+                        p.globalAvgQuantity
+                          ? percentageToColor(
+                              (p.globalAvgSum / p.globalAvgQuantity -
+                                minMax.min) /
+                                (minMax.max - minMax.min),
+                              theme as any
+                            )
+                          : "#666666"
+                      }`,
+                    }}
+                  >
+                    {p.globalAvgQuantity
+                      ? (p.globalAvgSum / p.globalAvgQuantity).toFixed(2)
+                      : "-"}
+                  </div>
                 </div>
-              </div>
+              </Link>
             </li>
           ))}
     </ul>
--- components/LatestSeasonRanking/LatestSeasonAwardRanking.tsx
+++ components/LatestSeasonRanking/LatestSeasonAwardRanking.tsx
@@ -38,29 +38,29 @@ const LatestSeasonAwardRanking = ({ stats }: LatestSeasonAwardRankingProps) => {
               <div className="flex items-center justify-center w-8 h-full bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-300 font-num">
                 {i + 1}
               </div>
-              <div className="flex items-center justify-between flex-1 ml-1 overflow-hidden bg-white border border-gray-100 rounded dark:bg-dark-500 dark:border-dark-300">
-                <Link href={`/players/${p.id}`}>
+              <Link href={`/players/${p.id}`}>
+                <div className="flex items-center justify-between flex-1 ml-1 overflow-hidden bg-white border border-gray-100 rounded cursor-pointer dark:bg-dark-500 dark:border-dark-300">
                   <span className="px-2">
                     {p.firstName[0] + ". " + p.lastName}
                   </span>
-                </Link>
-                <div
-                  className="flex items-center justify-center w-12 h-full bg-marine-100 dark:bg-dark-400 text-marine-600 dark:text-marine-400 font-num"
-                  style={{
-                    color: `${
-                      p.globalAvgQuantity
-                        ? percentageToColor(
-                            (p.globalAward - minMax.min) /
-                              (minMax.max - minMax.min),
-                            theme as any
-                          )
-                        : "#666666"
-                    }`,
-                  }}
-                >
-                  {p.globalAvgQuantity ? p.globalAward : "-"}
+                  <div
+                    className="flex items-center justify-center w-12 h-full bg-marine-100 dark:bg-dark-400 text-marine-600 dark:text-marine-400 font-num"
+                    style={{
+                      color: `${
+                        p.globalAvgQuantity
+                          ? percentageToColor(
+                              (p.globalAward - minMax.min) /
+                                (minMax.max - minMax.min),
+                              theme as any
+                            )
+                          : "#666666"
+                      }`,
+                    }}
+                  >
+                    {p.globalAvgQuantity ? p.globalAward : "-"}
+                  </div>
                 </div>
-              </div>
+              </Link>
             </li>
           ))}
     </ul>
