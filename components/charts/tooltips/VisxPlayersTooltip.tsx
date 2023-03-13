e67f84d9571badd5a22913820d5e07b608c076a8
kevscript <kevscripted@gmail.com>
2023-03-13 15:33:51 +0100

    updated tooltips

--- components/charts/tooltips/VisxPlayersTooltip.tsx
+++ components/charts/tooltips/VisxPlayersTooltip.tsx
@@ -1,3 +1,5 @@
+import HomeIcon from "@/components/Icons/HomeIcon";
+import PlaneIcon from "@/components/Icons/PlaneIcon";
 import { FormattedPlayersChartData } from "@/utils/charts/formatPlayersChartData";
 import { getContrastColor } from "@/utils/getContrastColor";
 import { TooltipWithBounds } from "@visx/tooltip";
@@ -44,19 +46,23 @@ const VisxPlayersTooltip = ({
         <div className="flex w-full flex-nowrap gap-x-[1px]">
           <div className="flex flex-col flex-nowrap gap-y-[1px]">
             <div className="flex justify-between w-full gap-x-[1px]">
+              <div className="flex items-center justify-center px-2 py-1 text-xs bg-gray-100">
+                {data.match.home ? (
+                  <HomeIcon className="w-3.5 h-3.5 fill-dark-600" />
+                ) : (
+                  <PlaneIcon className="w-3.5 h-3.5 fill-dark-600" />
+                )}
+              </div>
               <div
                 className="flex items-center justify-center flex-1 px-2 py-1 text-xs font-bold text-white"
                 style={{
-                  backgroundColor: data.match.opponent.primary,
+                  backgroundColor: `${data.match.opponent.primary}B3`,
                   color: data
                     ? getContrastColor(data.match.opponent.primary)
                     : "white",
                 }}
               >
                 {data.match.opponent.abbreviation}
-              </div>
-              <div className="justify-center px-2 py-1 text-xs bg-gray-100">
-                {data.match.home ? "H" : "A"}
               </div>
             </div>
             <div className="flex justify-between w-full gap-x-[1px]">
